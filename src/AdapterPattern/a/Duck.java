1f48cea4ce646971558c5efa026e30fa0d5fd8b3
leishiguang <shiguang_lei@126.com>
2018-10-19 11:40:54 +0800

    Add AdapterPattern

---
+++ src/AdapterPattern/a/Duck.java
@@ -0,0 +1,12 @@
+package AdapterPattern.a;
+
+/**
+ * @author LeiShiGuang
+ * Date  2018/10/19 9:54
+ * @version v1.0
+ */
+public interface Duck {
+    void quack();
+
+    void fly();
+}
---
+++ src/AdapterPattern/a/Turkey.java
@@ -0,0 +1,12 @@
+package AdapterPattern.a;
+
+/**
+ * @author LeiShiGuang
+ * Date  2018/10/19 9:55
+ * @version v1.0
+ */
+public interface Turkey {
+    void gobble();
+
+    void fly();
+}
