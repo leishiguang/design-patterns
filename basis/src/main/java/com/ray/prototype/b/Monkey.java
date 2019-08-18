927ea13f520db1da84e8edc8cf01af907e4f648f
leishiguang <leishiguang@outlook.com>
2019-08-18 23:59:59 +0800

    add prototype pattern deep clone

---
+++ basis/src/main/java/com/ray/prototype/b/Monkey.java
@@ -0,0 +1,14 @@
+package com.ray.prototype.b;
+
+import java.util.Date;
+
+/**
+ * @author leishiguang
+ * @version v1.0.0
+ * @since v1.0
+ */
+public class Monkey {
+    public int height;
+    public int weight;
+    public Date birthday;
+}
