e9bc1c2b8b700a90f27de5da8b5b7140b2bad948
leishiguang <leishiguang@outlook.com>
2019-08-21 23:42:41 +0800

    add template pattern

---
+++ basis/src/main/java/com/ray/template/course/JavaCourse.java
@@ -0,0 +1,16 @@
+package com.ray.template.course;
+
+/**
+ * java 课程
+ *
+ * @author leishiguang
+ * @version v1.0.0
+ * @since v1.0
+ */
+public class JavaCourse extends BaseNetworkCourse {
+    @Override
+    void checkHomework() {
+        System.out.println("检查java课程作业");
+    }
+
+}
